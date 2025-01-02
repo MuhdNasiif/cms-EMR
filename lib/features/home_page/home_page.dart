b3b2369a2679b7bd69bfc30f709bdfd455cdccb5
lulushadin <20mcs21@meaec.edu.in>
2025-01-02 11:46:32 +0530

    basics

--- lib/features/home_page/home_page.dart
+++ lib/features/home_page/home_page.dart
@@ -11,6 +11,9 @@ class HomePage extends StatelessWidget {
           'EMR',
         ),
       ),
+      body: Column(
+        children: [Text('Electronic Medical Records ')],
+      ),
     );
   }
 }
