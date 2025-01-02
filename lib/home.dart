e2e4b647c09e7d74c2a9e32e29d34fe61655881e
MuhdNasiif <nasiif.official@gmail.com>
2025-01-02 10:37:34 +0530

    asdf

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Homedemo extends StatelessWidget {
+  const Homedemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Column(
+        children: [
+          Center(child: Text("data"),
+          
+    
+          ),
+          Text("data")
+        ],
+      ),
+    );
+  }
+}
