9675b391ab3026c58cb3e5c373727c106c5ef20a
lulushadin <20mcs21@meaec.edu.in>
2025-01-02 11:23:24 +0530

    basics

---
+++ lib/components/app_bottom.dart
@@ -0,0 +1,59 @@
+import 'package:cms_emrgroup/features/doctors_page/doctors_page.dart';
+import 'package:cms_emrgroup/features/home_page/home_page.dart';
+import 'package:cms_emrgroup/features/patients_page/patient_page.dart';
+import 'package:cms_emrgroup/features/settings_page/settings_page.dart';
+import 'package:cms_emrgroup/utils/app_color.dart';
+import 'package:flutter/material.dart';
+
+class AppBottomBar extends StatefulWidget {
+  const AppBottomBar({
+    super.key,
+  });
+
+  @override
+  State<AppBottomBar> createState() => _AppBottomBarState();
+}
+
+class _AppBottomBarState extends State<AppBottomBar> {
+  int currentIndex = 0;
+
+  final List<Widget> _pages = [
+    HomePage(),
+    DoctorsPage(),
+    PatientPage(),
+    SettingsPage()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    _pages[0] = HomePage();
+    return Scaffold(
+        body: _pages[currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: primarycolor,
+            currentIndex: currentIndex,
+            onTap: (index) {
+              setState(() {
+                currentIndex = index;
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.people_outline),
+                label: 'Patients',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person_2_outlined),
+                label: 'Doctors',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+              ),
+            ]));
+  }
+}
